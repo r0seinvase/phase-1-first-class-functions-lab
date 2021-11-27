387aa3798a2b4c43fec41fb06fff0cfb80f54442
Paola Moreno Reyes <nailamoreno99@gmail.com>
2021-11-27 10:49:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+
+const returnFirstTwoDrivers = function(array) {return array.slice(0,2)};
+// this anonymous function returns lthe first two drivers by cutting of asset two, 
+// and anything that comes after 
+const returnLastTwoDrivers = function(array) {return array.slice(-2)};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (fare) {
+let multiplier = fare;
+return function(fare) {
+return multiplier*fare;
+}
+}
+function fareDoubler(fare) {
+    return fare*2;
+}
+function fareTripler(fare) {
+    return fare*3;
+}
+function selectDifferentDrivers(array,select) {
+    if (select === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(array);
+    } else if (select === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(array);
+    }
+}
